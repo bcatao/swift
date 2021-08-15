f8d6f8f2a2ee5e1f6729bbcbca65b04d2a80f3fb
bcatao <60741558+bcatao@users.noreply.github.com>
2021-08-15 17:29:19 -0300

    Update ContentView.swift

--- Connections/Connections/ContentView.swift
+++ Connections/Connections/ContentView.swift
@@ -9,26 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(spacing:10){
-            VStack (spacing: 20){
-            ConnectionView(imageName: "monica")
-            ConnectionView(imageName: "rachel")
-                
-            }
         
-        VStack(spacing: 20){
-            ConnectionView(imageName: "joey")
-            ConnectionView(imageName: "yourname")
-            ConnectionView(imageName: "chandler")
-        }
-            VStack (spacing: 20){
-            ConnectionView(imageName: "phoebe")
-            ConnectionView(imageName: "ross")
-                
-            }
-    }
+        ZStack{
+        
+        Divider().rotationEffect(.degrees(30))
+        Divider().rotationEffect(.degrees(90))
+        Divider().rotationEffect(.degrees(-30))
+        
+        ConnectionView(imageName: "yourname")
+		
+			HStack{
+			
+			ConnectionView(imageName: "monica")
+			.rotationEffect(.degrees(-30))
+		Spacer()
+				ConnectionView(imageName: "ross")
+					.rotationEffect(.degrees(-30))
+			}.rotationEffect(.degrees(30))
+			
+			HStack{
+		ConnectionView(imageName: "joey")
+			.rotationEffect(.degrees(-90))
+				Spacer()
+		ConnectionView(imageName: "chandler")
+			.rotationEffect(.degrees(-90))
+				
+			}.rotationEffect(.degrees(90))
+		
+			HStack{
+		ConnectionView(imageName: "rachel")
+			.rotationEffect(.degrees(30))
+				Spacer()
+				ConnectionView(imageName: "phoebe")
+					.rotationEffect(.degrees(30))
+			}.rotationEffect(.degrees(-30))
+                      
+		}.frame(width: 300, height: 300)
     
     }
     
-}
-            
+    }
